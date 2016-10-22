40d4047368022fbf21ec7d04cdc056aff2d41ff1
Aditi.Toshniwal <aditi.t@media.net>
2016-10-22 18:32:58 +0530

    Add git oauth

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var mongo = require("mongodb").MongoClient;
+var request = require("request");
 var settings = require("./settings.js");
+var cors = require("cors");
 
 var app = express();
 
 var db = null;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get("/", function (req, res) {
     res.send("Sup lol!");
@@ -18,6 +21,7 @@ app.post("/webhook", function (req, res) {
     var type = req.body.name;
 
     if (type === "app.install") {
+
         var newUser = {
             _id: req.body.userId,
             userToken: req.body.userToken,
@@ -33,7 +37,9 @@ app.post("/webhook", function (req, res) {
                 res.sendStatus(200);
             }
         });
+
     } else if (type === "app.uninstall") {
+
         db.collection("users").update({_id: req.body.userId}, {$set: {installed: false}}, function (uninstallErr) {
             if (uninstallErr) {
                 console.error("Error setting user uninstalled", req.body.userId);
@@ -43,14 +49,57 @@ app.post("/webhook", function (req, res) {
                 res.sendStatus(200);
             }
         });
-    }
 
-    else if (type === "client.slashCommand"){
-        if(req.body.text === "p p"){
+    } else if (type === "client.slashCommand") {
+
+        if (req.body.text === "p p"){
             var response = {text: "Processing..."};
             res.status(200).json(response);
         }
+
+        request(flockPost({
+            "text": "Authorize GitKit to access your Github account: " + gitOAuth(req.body.userId)
+            // "attachments": [{
+            //     "title": "Authorize GitHub",
+            //     "description": "Do it!",
+            //     "views": {
+            //         // "widget": { "src": "https://google.com", "width": 1000, "height": 400 }
+            //         // "flockml": '<flockml><action "type"="openBrowser" "url"="' + gitOAuth + '">Authorize GitKit</action> to access your GitHub account</flockml>'
+            //     }
+            //     "buttons": [{
+            //         "name": "Authorize on GitHub",
+            //         "action": {
+            //             "type": "openWidget",
+            //             "url": gitOAuth(),
+            //             "desktopType": "sidebar",
+            //             "mobileType": "modal"
+            //         },
+            //         "id": "dettol_peele"
+            //     }]
+            // }]
+        }), function (error, response, body) {
+            if (error) {
+                console.error("Error starting git auth flow", error);
+            }
+        });
+
     }
+});
+
+app.get('/github-auth/:userid', function (req, res) {
+    request(gitTokenPost(req.query.code), function (error, response, body) {
+        if (error) {
+            console.error("Error getting git auth token", error);
+        }
+        console.log(body);
+        res.send("You have authorized GitKit to access your Github account. You may now close this window.");
+
+        db.collection("users").update({_id: req.params.userid}, {$set: {githubToken: body.access_token}}, function (tokenInsertError) {
+            if (tokenInsertError) {
+                console.log("Error inserting github token", tokenInsertError);
+            }
+        });
+    });
 });
 
 mongo.connect(settings.mongoUrl, function (mongoConnectErr, connectedDb) {
@@ -70,3 +119,31 @@ mongo.connect(settings.mongoUrl, function (mongoConnectErr, connectedDb) {
         console.log("Started server");
     });
 });
+
+var flockPost = function (body) {
+    return {
+        url: settings.incomingWebhookUrl,
+        method: 'POST',
+        json: body
+    };
+};
+
+var gitOAuth = function (userid) {
+    return "https://github.com/login/oauth/authorize?client_id=" + settings.githubClientId
+            + "&redirect_uri=https://a9291a1d.ngrok.io/github-auth/" + userid + "&scope=repo:status"
+            + "&state=d5e497f8-54ff-5924-843d-dfcb52da9f7a&allow_signup=true";
+};
+
+var gitTokenPost = function (code) {
+    return {
+        uri: "https://github.com/login/oauth/access_token",
+        method: "POST",
+        json: {
+            client_id: settings.githubClientId,
+            client_secret: settings.githubClientSecret,
+            code: code,
+            redirect_uri: "https://a9291a1d.ngrok.io/github-auth",
+            state: "d5e497f8-54ff-5924-843d-dfcb52da9f7a"
+        }
+    };
+};
--- settings.js
+++ settings.js
@@ -1,6 +1,8 @@
 module.exports = {
-	incomingWebhookUrl: "https://api.flock.co/hooks/sendMessage/57112208-178a-4a29-afdb-cb39db3a4207",
-	appId: "67e442e9-223a-4c3c-a06a-5d85b5aba938",
-	appSecret: "0d7fbf20-eb99-4a5b-822c-ebfd8e8a153d",
-	mongoUrl: "mongodb://localhost:27017/gitkit"
+    incomingWebhookUrl: "https://api.flock.co/hooks/sendMessage/57112208-178a-4a29-afdb-cb39db3a4207",
+    flockAppId: "67e442e9-223a-4c3c-a06a-5d85b5aba938",
+    flockAppSecret: "0d7fbf20-eb99-4a5b-822c-ebfd8e8a153d",
+    mongoUrl: "mongodb://localhost:27017/gitkit",
+    githubClientId: "9e917b3311cba7183292",
+    githubClientSecret: "a0f68f7b6c95dd10e7cfdbffce6131b72b872e3b"
 };
