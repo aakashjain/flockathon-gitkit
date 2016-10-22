bd4a1bb62dc40ac6b885549b76207f920f489730
Aditi.Toshniwal <aditi.t@media.net>
2016-10-23 00:20:32 +0530

    Add buncha shit idk lol

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ var mongo = require("mongodb").MongoClient;
 var request = require("request");
 var settings = require("./settings.js");
 var cors = require("cors");
+var async = require("async");
+var Chance = require("chance");
+var chance = new Chance();
+var fs = require("fs");
 
 var app = express();
 
@@ -24,7 +28,6 @@ app.post("/webhook", function (req, res) {
 
         var newUser = {
             _id: req.body.userId,
-            userToken: req.body.userToken,
             token: req.body.token,
             installed: true
         };
@@ -52,35 +55,50 @@ app.post("/webhook", function (req, res) {
 
     } else if (type === "client.slashCommand") {
 
-        if (req.body.text === "p p"){
-            var response = {text: "Processing..."};
-            res.status(200).json(response);
-        }
+        res.status(200).json({text: "Processing..."});
 
-        request(flockPost({
-            "text": "Authorize GitKit to access your Github account: " + gitOAuth(req.body.userId)
-            // "attachments": [{
-            //     "title": "Authorize GitHub",
-            //     "description": "Do it!",
-            //     "views": {
-            //         // "widget": { "src": "https://google.com", "width": 1000, "height": 400 }
-            //         // "flockml": '<flockml><action "type"="openBrowser" "url"="' + gitOAuth + '">Authorize GitKit</action> to access your GitHub account</flockml>'
-            //     }
-            //     "buttons": [{
-            //         "name": "Authorize on GitHub",
-            //         "action": {
-            //             "type": "openWidget",
-            //             "url": gitOAuth(),
-            //             "desktopType": "sidebar",
-            //             "mobileType": "modal"
-            //         },
-            //         "id": "dettol_peele"
-            //     }]
-            // }]
-        }), function (error, response, body) {
-            if (error) {
-                console.error("Error starting git auth flow", error);
+        db.collection("users").findOne({_id: req.body.userId}, function (err, doc) {
+            if (err) {
+                console.log("Error finding user with id", req.body.userId, err);
             }
+
+            if (doc.githubToken == undefined) {
+                request(flockPost({
+                    "text": "Authorize GitKit to access your Github account: ",
+                    "to": req.body.chat,
+                    "token": settings.flockBotToken,
+                    "attachments": [{
+                        // "views": {
+                        //     "image": {
+                        //         "original": {
+                        //             "src": "https://image.shutterstock.com/z/stock-photo-top-view-of-architect-drawing-on-architectural-project-347420135.jpg"
+                        //         }
+                        //     }
+                        // },
+                        "buttons": [{
+                            "name": "Authorize on GitHub",
+                            "action": {
+                                "type": "openBrowser",
+                                "url": gitOAuth(req.body.userId)
+                            },
+                            "id": "auth_button"
+                        }]
+                    }]
+                }), function (error, response, body) {
+                    if (error) {
+                        console.error("Error starting git auth flow", error);
+                    }
+                });
+                
+                return;
+            }
+
+            if (req.body.text === "commit map") {
+
+            } else if (req.body.text === "commit repos") {
+                fetchUserCommitsOnRepos(doc, req.body.chat);
+            }
+
         });
 
     }
@@ -94,9 +112,29 @@ app.get('/github-auth/:userid', function (req, res) {
         console.log(body);
         res.send("You have authorized GitKit to access your Github account. You may now close this window.");
 
-        db.collection("users").update({_id: req.params.userid}, {$set: {githubToken: body.access_token}}, function (tokenInsertError) {
-            if (tokenInsertError) {
-                console.log("Error inserting github token", tokenInsertError);
+        request({
+            method: "GET",
+            uri: "https://api.github.com/user",
+            headers: {
+                authorization: "token " + body.access_token,
+                'User-Agent': 'Chrome/51.0.2704.103'
+            }
+        }, function (err, response, body2) {
+            if (err) {
+                console.error("Error fetching user info from Github", err);
+            } else {
+                body2 = JSON.parse(body2);
+                var updates = {
+                    $set: {
+                        githubToken: body.access_token,
+                        githubUser: body2.login
+                    }
+                };
+                db.collection("users").update({_id: req.params.userid}, updates, function (tokenInsertError) {
+                    if (tokenInsertError) {
+                        console.log("Error inserting github token", tokenInsertError);
+                    }
+                });
             }
         });
     });
@@ -122,7 +160,7 @@ mongo.connect(settings.mongoUrl, function (mongoConnectErr, connectedDb) {
 
 var flockPost = function (body) {
     return {
-        url: settings.incomingWebhookUrl,
+        url: settings.flockPostUrl,
         method: 'POST',
         json: body
     };
@@ -130,7 +168,7 @@ var flockPost = function (body) {
 
 var gitOAuth = function (userid) {
     return "https://github.com/login/oauth/authorize?client_id=" + settings.githubClientId
-            + "&redirect_uri=https://a9291a1d.ngrok.io/github-auth/" + userid + "&scope=repo:status"
+            + "&redirect_uri=" + settings.ngrokUrl + "/github-auth/" + userid + "&scope=repo:status"
             + "&state=d5e497f8-54ff-5924-843d-dfcb52da9f7a&allow_signup=true";
 };
 
@@ -142,8 +180,117 @@ var gitTokenPost = function (code) {
             client_id: settings.githubClientId,
             client_secret: settings.githubClientSecret,
             code: code,
-            redirect_uri: "https://a9291a1d.ngrok.io/github-auth",
+            redirect_uri: settings.ngrokUrl + "/github-auth",
             state: "d5e497f8-54ff-5924-843d-dfcb52da9f7a"
         }
     };
 };
+
+var fetchUserCommitsOnRepos = function (user, chatId) {
+    var githubUser = user.githubUser;
+    var githubToken = user.githubToken;
+    var commits = [];
+    request({
+        method: "GET",
+        uri: "https://api.github.com/users/" + githubUser+ "/repos",
+        headers: {
+            authorization: "token " + githubToken,
+            "User-Agent": "Chrome/51.0.2704.103"
+        }
+    }, function (err, response, body) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        body = JSON.parse(body);
+        async.eachSeries(body, function (repo, cb) {
+            var reponame = repo.full_name;
+            request({
+                method: "GET",
+                uri: "https://api.github.com/repos/" + reponame + "/commits",
+                qs: { author: githubUser },
+                headers: {
+                    authorization: "token " + githubToken,
+                    "User-Agent": "Chrome/51.0.2704.103"
+                }
+            }, function (err, response, body) {
+                if (err) {
+                    console.error(err);
+                    return cb();   
+                }
+                body = JSON.parse(body);
+                body.forEach(function (commit) {
+                    commits.push({
+                        repo: reponame,
+                        date: commit.commit.committer.date
+                    });
+                });
+                cb();
+            });
+
+        }, function (err) {
+
+            createAndSendChart(commits, "userCommitRepos.js", chatId);
+            
+        });
+    });
+};
+
+var createAndSendChart = function (data, script, chatId) {
+    var chart = {
+        _id: chance.guid(),
+        data: JSON.stringify(data, null, 0),
+        script: script
+    };
+
+    db.collection("charts").insert(chart, function (err) {
+        if (err) {
+            console.error("Error creating new chart", err);
+            return;
+        }
+
+        request(flockPost({
+            to: chatId,
+            token: settings.flockBotToken,        
+            text: "Here's the chart!",
+            attachments: [{
+                buttons: [{
+                    "name": "View chart",
+                    "action": {
+                        "type": "openWidget",
+                        "url": settings.ngrokUrl + "/charts/" + chart._id,
+                        "desktopType": "modal",
+                        "mobileType": "modal"
+                    },
+                    "id": "chart_button"
+                }]
+            }]
+        }), function (err, response, body) {
+            if (err) {
+                console.error("Error posting chart url", err);
+            }
+        });
+    });
+};
+
+app.get("/charts/:chartId", function (req, res) {
+    db.collection("charts").findOne({_id: req.params.chartId}, function (err, doc) {
+        if (err) {
+            console.log("Error fetching chart with id", req.params.chartId, err);
+            return res.sendStatus(500);
+        }
+
+        if (doc == null) {
+            return res.send("This chart has expired.");
+        }
+
+        var html = fs.readFileSync("charts/chart.html", {encoding: "utf8"});
+        var js = fs.readFileSync("charts/" + doc.script, {encoding: "utf8"});
+        js = "var rawDataString = " + doc.data + ";" + js;
+        html =  html.replace("$$SCRIPT$$", js);
+
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(html);
+        res.end();
+    });
+});
--- settings.js
+++ settings.js
@@ -1,8 +1,11 @@
 module.exports = {
     incomingWebhookUrl: "https://api.flock.co/hooks/sendMessage/57112208-178a-4a29-afdb-cb39db3a4207",
+    flockPostUrl: "https://api.flock.co/v1/chat.sendMessage",
     flockAppId: "67e442e9-223a-4c3c-a06a-5d85b5aba938",
     flockAppSecret: "0d7fbf20-eb99-4a5b-822c-ebfd8e8a153d",
     mongoUrl: "mongodb://localhost:27017/gitkit",
     githubClientId: "9e917b3311cba7183292",
-    githubClientSecret: "a0f68f7b6c95dd10e7cfdbffce6131b72b872e3b"
+    githubClientSecret: "a0f68f7b6c95dd10e7cfdbffce6131b72b872e3b",
+    ngrokUrl: "https://a9291a1d.ngrok.io",
+    flockBotToken: "41ca5cdc-3969-4860-925d-f9e8f8ba01d9"
 };
